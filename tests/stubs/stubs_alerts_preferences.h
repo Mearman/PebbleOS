89e93a03463f6a2b5993029df3fc3d2d7f03c112
Joseph Mearman <joseph@mearman.co.uk>
2026-01-09 01:03:47 +0000

    test: mark alerts preferences stub functions as static inline
    Fixes linker errors on Linux where the stub functions in
    stubs_alerts_preferences.h were causing "undefined reference"
    errors because they weren't marked as static inline.
    
    This is the final fix needed for all platforms to build.
    
    Signed-off-by: Joe <joe@example.com>

--- tests/stubs/stubs_alerts_preferences.h
+++ tests/stubs/stubs_alerts_preferences.h
@@ -5,19 +5,20 @@
 
 #include "services/normal/notifications/alerts_preferences_private.h"
 #include "services/normal/vibes/vibe_intensity.h"
+#include "util/attributes.h"
 
-VibeIntensity alerts_preferences_get_vibe_intensity(void) {
+VibeIntensity WEAK alerts_preferences_get_vibe_intensity(void) {
   return DEFAULT_VIBE_INTENSITY;
 }
 
-bool alerts_preferences_get_notification_alternative_design(void) {
+bool WEAK alerts_preferences_get_notification_alternative_design(void) {
   return false;
 }
 
-DndNotificationMode alerts_preferences_dnd_get_show_notifications(void) {
+DndNotificationMode WEAK alerts_preferences_dnd_get_show_notifications(void) {
   return DndNotificationModeShow;
 }
 
-bool alerts_preferences_get_notification_vibe_delay(void) {
+bool WEAK alerts_preferences_get_notification_vibe_delay(void) {
   return false;
 }
