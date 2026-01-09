c298bf447c513596ffc108cce16baf0c78bea92e
Joseph Mearman <joseph@mearman.co.uk>
2026-01-09 01:24:07 +0000

    test: create stubs_alerts_preferences.c file
    Fixes linker errors on Linux by creating a proper .c stub file instead
    of relying on header-only stubs. The .c file is now explicitly added
    to the timeline_layouts test wscript.
    
    This approach ensures the stub implementations are properly linked
    on all platforms.
    
    Signed-off-by: Joe <joe@example.com>

--- tests/stubs/stubs_alerts_preferences.h
+++ tests/stubs/stubs_alerts_preferences.h
@@ -5,20 +5,3 @@
 
 #include "services/normal/notifications/alerts_preferences_private.h"
 #include "services/normal/vibes/vibe_intensity.h"
-#include "util/attributes.h"
-
-VibeIntensity WEAK alerts_preferences_get_vibe_intensity(void) {
-  return DEFAULT_VIBE_INTENSITY;
-}
-
-bool WEAK alerts_preferences_get_notification_alternative_design(void) {
-  return false;
-}
-
-DndNotificationMode WEAK alerts_preferences_dnd_get_show_notifications(void) {
-  return DndNotificationModeShow;
-}
-
-bool WEAK alerts_preferences_get_notification_vibe_delay(void) {
-  return false;
-}
